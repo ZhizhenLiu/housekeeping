34dd420ef0d34ad474111e1c9611a730abe321a4
zhizhenLiu <13549511517@163.com>
2021-03-05 11:15:33 +0800

    'mybatis-generator使用'

---
+++ src/main/java/com/housekeeping/config/MybatisConfig.java
@@ -0,0 +1,10 @@
+package com.housekeeping.config;
+
+/**
+ * description: MybatisConfig
+ * date: 2021/3/5 11:07
+ * author: zhizhenLiu
+ * version: 1.0
+ */
+public class MybatisConfig {
+}
---
+++ src/main/java/com/housekeeping/dao/extend/CategoryExtendMapper.java
@@ -0,0 +1,7 @@
+package com.housekeeping.dao.extend;/**
+ * description: CategoryExtendMapper 
+ * date: 2021/3/5 10:10 
+ * author: zhizhenLiu 
+ * version: 1.0 
+ */public class CategoryExtendMapper {
+}
